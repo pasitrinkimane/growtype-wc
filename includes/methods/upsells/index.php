6cb7538c60a7683127e9aa41e0d34f68fecc2936
Vytautas Dirzauskas <newcoolstudio@gmail.com>
2026-04-18 11:17:42 +0300

    overal fixes

--- includes/methods/upsells/index.php
+++ includes/methods/upsells/index.php
@@ -9,6 +9,7 @@ class Growtype_Wc_Upsell
     const MODAL_ID = 'growtypeWcUpsellModal';
     const DISMISS_AJAX_ACTION = 'growtype_wc_dismiss_upsell';
     const GET_ITEM_AJAX_ACTION = 'growtype_wc_get_upsell_item';
+    const FORCE_TRIGGER_QUERY_PARAM = 'gwcupsells';
 
     public function __construct()
     {
